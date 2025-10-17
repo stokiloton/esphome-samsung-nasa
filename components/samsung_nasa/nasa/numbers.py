c793a0d4182d92913e9d78254de8e33154c49881
stokiloton <stokiloton@gmail.com>
2025-10-17 10:29:34 +0200

    Update numbers.py

--- components/samsung_nasa/nasa/numbers.py
+++ components/samsung_nasa/nasa/numbers.py
@@ -104,7 +104,7 @@ numbers = {
     0x4247: {
         NASA_LABEL: "VAR_IN_TEMP_WATER_OUTLET_TARGET_F",
         NASA_MODE: CONTROLLER_MODE_CONTROL,
-        CONF_DATA: cmd_numeric_data(15, 55),
+        CONF_DATA: cmd_numeric_data(25, 70, 0.5),
         CONF_DEFAULTS: temperature_defaults()
     },
     0x4248: {
